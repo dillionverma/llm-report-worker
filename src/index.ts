42b055606ae1e92a99fdaf707486f1ac9692102f
Dillion Verma <hello@dillionverma.com>
2023-05-14 02:22:02 -0400

    🐛 fix: check for API key and user before saving

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client/edge";
+
 const prisma = new PrismaClient();
 
 const CACHE_AGE = 60 * 60 * 24 * 30; // 30 days
@@ -9,7 +10,7 @@ addEventListener("fetch", (event) => {
 
 async function sha256(message: string) {
   // encode as UTF-8
-  const msgBuffer = await new TextEncoder().encode(message);
+  const msgBuffer = new TextEncoder().encode(message);
   // hash the message
   const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
   // convert bytes to hex string
@@ -26,31 +27,24 @@ const getUrl = async (request: Request) => {
   return openaiUrl;
 };
 
-async function callOpenAI({
-  url,
-  method,
-  headers,
-  body,
-  request,
-}: {
-  method: string;
-  url: string;
-  headers: Headers;
-  body?: string;
-  request: Request;
-}): Promise<Response> {
-  let response: Response;
+const getCompletionFromStream = (stream: string): string => {
+  if (!stream) return "";
+  const events = stream
+    .split("\n")
+    .filter((e) => e.length > 0)
+    .slice(0, -1); // cut off the last one
 
-  if (method === "POST") {
-    response = await fetch(url, request);
-    return response;
-  } else if (method === "GET") {
-    response = await fetch(url, request);
-    return response;
-  } else {
-    return new Response("Method not allowed", { status: 405 });
+  let completion = "";
+
+  for (const event of events) {
+    const json = event.replace("data: ", "");
+    const parsed = JSON.parse(json);
+
+    completion += parsed.choices[0].delta.content || "";
   }
-}
+
+  return completion;
+};
 
 interface OpenAIResponse {
   id: string;
@@ -66,44 +60,68 @@ const saveRequestToDb = async (
   url: string,
   body: { [key: string]: any },
   metadata: { [key: string]: any },
-  cached: boolean = false
+  cached: boolean = false,
+  streamed: boolean = false,
+  userId: string,
+  data?: { [key: string]: any },
+  streamed_data?: string
 ) => {
-  const data: OpenAIResponse = await response.json();
+  let streamed_id: string = "";
+  let completion: string = "";
 
-  // waitUntil method is used for sending logs, after response is sent
-  const r = prisma.request.create({
-    data: {
-      openai_id: data.id,
-      ip: request.headers.get("x-real-ip") || "",
-      url: url,
-      method: request.method,
-      status: response.status,
-      request_headers: JSON.stringify(
-        Object.fromEntries(request.headers.entries())
-      ),
-      request_body: JSON.stringify(body),
-      response_headers: JSON.stringify(
-        Object.fromEntries(response.headers.entries())
-      ),
-      response_body: JSON.stringify(data),
+  if (streamed) {
+    const data = streamed_data?.split("\n\n")[0].replace("data: ", "");
+    const parsed = JSON.parse(data!);
+    streamed_id = parsed.id;
+    console.log("Streamed id:", streamed_id);
+  }
 
-      cached: cached,
+  if (url === "https://api.openai.com/v1/chat/completions") {
+    if (streamed) {
+      completion = getCompletionFromStream(streamed_data!);
+    } else {
+      completion = data?.choices[0].message.content;
+    }
+  } else if (url === "https://api.openai.com/v1/completions") {
+    completion = data?.choices[0].text;
+  }
 
-      // prompt_tokens: data.usage.prompt_tokens,
-      // completion_tokens: data.usage.completion_tokens,
-      // total_tokens: data.usage.total_tokens,
-      metadata: {
-        create: [
-          ...Object.entries(metadata || {}).map(([key, value]) => ({
-            key: key,
-            value: value,
-          })),
-        ],
+  try {
+    const r = prisma.request.create({
+      data: {
+        openai_id: streamed ? streamed_id : data?.id,
+        ip: request.headers.get("x-real-ip") || "",
+        url: url,
+        method: request.method,
+        status: response.status,
+        request_headers: Object.fromEntries(request.headers.entries()),
+        request_body: body,
+        response_headers: Object.fromEntries(response.headers.entries()),
+
+        response_body: streamed ? undefined : data,
+        streamed_response_body: streamed ? streamed_data : undefined,
+
+        cached: cached,
+        streamed: streamed,
+        completion: completion,
+
+        metadata: {
+          create: [
+            ...Object.entries(metadata || {}).map(([key, value]) => ({
+              key: key,
+              value: value,
+            })),
+          ],
+        },
+        userId: userId,
       },
-    },
-  });
+    });
 
-  return r;
+    return r;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
 
 const logHeaders = async (headers: Headers) => {
@@ -113,6 +131,28 @@ const logHeaders = async (headers: Headers) => {
   );
 };
 
+const getApiKey = async (request: Request) => {
+  const headers = request.headers;
+  const authHeader = headers.get("X-Api-Key");
+  if (!authHeader) return null;
+
+  const apiKey = authHeader.replace("Bearer ", "");
+  return apiKey;
+};
+
+const getUser = async (apiKey: string) => {
+  const key = await prisma.apiKey.findUnique({
+    where: {
+      hashed_key: await sha256(apiKey),
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  return key?.user;
+};
+
 async function handleEvent(event: FetchEvent): Promise<Response> {
   const { request } = event;
   const headers = request.headers;
@@ -120,6 +160,40 @@ async function handleEvent(event: FetchEvent): Promise<Response> {
   const method = request.method;
 
   const url = await getUrl(request);
+
+  const key = await getApiKey(request);
+
+  if (!key) {
+    return new Response(
+      JSON.stringify({
+        message: "Go to https://llm.report/ to get an API key.",
+        error: "Missing API key in X-Api-Key header.",
+      }),
+      {
+        status: 401,
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      }
+    );
+  }
+
+  const user = await getUser(key);
+
+  if (!user) {
+    return new Response(
+      JSON.stringify({
+        message: "Go to https://llm.report/ to get an API key.",
+        error: "User not found.",
+      }),
+      {
+        status: 401,
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      }
+    );
+  }
 
   let metadata: { [key: string]: string } = {};
   let restBody: { [key: string]: any } = {};
@@ -158,39 +232,86 @@ async function handleEvent(event: FetchEvent): Promise<Response> {
       const headers = new Headers(initialResponse.headers);
       headers.set("cache-control", `public, max-age=${CACHE_AGE}`);
 
-      logHeaders(initialResponse.headers);
+      logHeaders(headers);
 
-      try {
-        response = new Response(initialResponse.body, {
-          status: initialResponse.status,
-          statusText: initialResponse.statusText,
-          headers,
-        });
+      // Create a new response with the transformed readable stream
+      response = new Response(initialResponse.body, {
+        status: initialResponse.status,
+        statusText: initialResponse.statusText,
+        headers,
+      });
 
-        // if (headers.get("llm-cache-enabled") === "true") {
-        // console.log("Caching enabled");
-        event.waitUntil(cache.put(cacheKey, response.clone()));
-      } catch (e) {
-        console.error("Error: ", e);
-      }
+      // if (headers.get("llm-cache-enabled") === "true") {
+      // console.log("Caching enabled");
+      event.waitUntil(cache.put(cacheKey, response.clone()));
+      // } catch (e) {
+      //   console.error("Error: ", e);
+      // }
       // }
     } else {
       cached = true;
       console.log("hit");
     }
 
-    event.waitUntil(
-      saveRequestToDb(
-        request,
-        response.clone(),
-        url,
-        JSON.parse(body),
-        metadata,
-        cached
-      )
-    );
+    const isStream = JSON.parse(body).stream === true;
 
-    return response;
+    if (isStream) {
+      const c = response.clone();
+      const reader = c.body.getReader();
+      const decoder = new TextDecoder();
+
+      let responseData = "";
+
+      reader.read().then(async function process({ done, value }): Promise<any> {
+        if (done) {
+          // console.log("Stream complete. Result:");
+          // console.log(responseData);
+          // Store responseData in your database
+          event.waitUntil(
+            saveRequestToDb(
+              request,
+              c,
+              url,
+              JSON.parse(body),
+              metadata,
+              cached,
+              true,
+              user.id,
+              undefined,
+              responseData
+            )
+          );
+          return;
+        }
+
+        const text = decoder.decode(value, { stream: true });
+        console.log(text);
+
+        responseData += text;
+        return reader.read().then(process);
+      });
+
+      return response;
+    } else {
+      console.log("Stream is false");
+
+      const c = response.clone();
+
+      event.waitUntil(
+        saveRequestToDb(
+          request,
+          c,
+          url,
+          JSON.parse(body),
+          metadata,
+          cached,
+          false,
+          user.id,
+          await c.json()
+        )
+      );
+      return response;
+    }
   }
 
   return new Response("Method not allowed", { status: 405 });
